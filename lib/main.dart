7d272f1404fa8b0715e896b16d5d71b0a4f70801
Uriel Aispuro <urielalexis64@gmail.com>
2021-06-20 22:04:14 -0600

    install app dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Material App',
       home: Scaffold(
         appBar: AppBar(
