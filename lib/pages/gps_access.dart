c7f704a0751663df945bb33170de771b79767491
Uriel Aispuro <urielalexis64@gmail.com>
2021-06-21 20:03:29 -0600

    detect lifecycle changes

--- lib/pages/gps_access.dart
+++ lib/pages/gps_access.dart
@@ -8,7 +8,28 @@ class GpsAccessPage extends StatefulWidget {
   _GpsAccessPageState createState() => _GpsAccessPageState();
 }
 
-class _GpsAccessPageState extends State<GpsAccessPage> {
+class _GpsAccessPageState extends State<GpsAccessPage>
+    with WidgetsBindingObserver {
+  @override
+  void initState() {
+    WidgetsBinding.instance!.addObserver(this);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance!.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) async {
+    if (state == AppLifecycleState.resumed &&
+        await Permission.location.isGranted) {
+      Navigator.pushReplacementNamed(context, 'loading');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- lib/pages/loading_page.dart
+++ lib/pages/loading_page.dart
@@ -2,33 +2,75 @@ import 'package:flutter/material.dart';
 import 'package:flutter_maps_app/helpers/helpers.dart';
 import 'package:flutter_maps_app/pages/gps_access.dart';
 import 'package:flutter_maps_app/pages/map_page.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:permission_handler/permission_handler.dart';
 
-class LoadingPage extends StatelessWidget {
+class LoadingPage extends StatefulWidget with WidgetsBindingObserver {
   const LoadingPage({Key? key}) : super(key: key);
+
+  @override
+  _LoadingPageState createState() => _LoadingPageState();
+}
+
+class _LoadingPageState extends State<LoadingPage> with WidgetsBindingObserver {
+  @override
+  void initState() {
+    WidgetsBinding.instance!.addObserver(this);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance!.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) async {
+    print(state);
+    if (state == AppLifecycleState.resumed &&
+        await Geolocator.isLocationServiceEnabled()) {
+      Navigator.pushReplacement(context, navigateFadeInMap(context, MapPage()));
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Scaffold(
         body: FutureBuilder(
-          future: checkGPS(context),
-          builder: (BuildContext context, AsyncSnapshot snapshot) => Center(
-            child: CircularProgressIndicator(
-              strokeWidth: 3,
-            ),
-          ),
+          future: checkGPS(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.hasData) {
+              return Center(
+                child: Text(snapshot.data),
+              );
+            }
+            return Center(
+              child: CircularProgressIndicator(
+                strokeWidth: 3,
+              ),
+            );
+          },
         ),
       ),
     );
   }
 
-  Future checkGPS(context) async {
+  Future checkGPS() async {
     // TODO: GPS Permission
+    final gpsPermission = await Permission.location.isGranted;
     // TODO: GPS is active
+    final gpsIsActive = await Geolocator.isLocationServiceEnabled();
 
-    await Future.delayed(Duration(milliseconds: 800));
-    Navigator.pushReplacement(
-        context, navigateFadeInMap(context, GpsAccessPage()));
-    //Navigator.pushReplacement(context, navigateFadeInMap(context, MapPage()));
+    if (gpsPermission && gpsIsActive) {
+      Navigator.pushReplacement(context, navigateFadeInMap(context, MapPage()));
+      return '';
+    } else if (!gpsPermission) {
+      Navigator.pushReplacement(
+          context, navigateFadeInMap(context, GpsAccessPage()));
+    } else {
+      return 'You need to activate your GPS.';
+    }
   }
 }
