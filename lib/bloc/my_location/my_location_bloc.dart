d3b74c7d33c5ca968c3fb3cb106564aacecf5312
Uriel Aispuro <urielalexis64@gmail.com>
2021-06-22 23:57:24 -0600

    start and cancel user location tracking

--- lib/bloc/my_location/my_location_bloc.dart
+++ lib/bloc/my_location/my_location_bloc.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:meta/meta.dart';
 
@@ -9,6 +10,20 @@ part 'my_location_state.dart';
 
 class MyLocationBloc extends Bloc<MyLocationEvent, MyLocationState> {
   MyLocationBloc() : super(MyLocationState());
+
+  StreamSubscription<Position>? _positionSubscription;
+
+  void startTracking() {
+    Geolocator.getPositionStream(
+            desiredAccuracy: LocationAccuracy.high, distanceFilter: 10)
+        .listen((Position position) {
+      print(position);
+    });
+  }
+
+  void cancelTracking() {
+    _positionSubscription?.cancel();
+  }
 
   @override
   Stream<MyLocationState> mapEventToState(
