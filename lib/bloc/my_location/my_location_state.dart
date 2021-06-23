7f0cb91515691fe8c100e5096bfdad5ce5fa5e49
Uriel Aispuro <urielalexis64@gmail.com>
2021-06-22 23:57:01 -0600

    change following to tracking

--- lib/bloc/my_location/my_location_state.dart
+++ lib/bloc/my_location/my_location_state.dart
@@ -2,17 +2,17 @@ part of 'my_location_bloc.dart';
 
 @immutable
 class MyLocationState {
-  final bool following;
+  final bool tracking;
   final bool locationExists;
   final LatLng? location;
 
   MyLocationState(
-      {this.following = true, this.locationExists = false, this.location});
+      {this.tracking = true, this.locationExists = false, this.location});
 
   MyLocationState copyWith(
           {bool? following, bool? locationExists, LatLng? location}) =>
       MyLocationState(
-        following: following ?? this.following,
+        tracking: following ?? this.tracking,
         locationExists: locationExists ?? this.locationExists,
         location: location ?? this.location,
       );
