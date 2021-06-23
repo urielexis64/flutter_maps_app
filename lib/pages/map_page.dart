63f937750c9992a266994b3ea96a97b0ee957168
Uriel Aispuro <urielalexis64@gmail.com>
2021-06-22 23:56:48 -0600

    start and cancel user location tracking

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,7 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_maps_app/bloc/my_location/my_location_bloc.dart';
 
-class MapPage extends StatelessWidget {
+class MapPage extends StatefulWidget {
   const MapPage({Key? key}) : super(key: key);
+
+  @override
+  _MapPageState createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  @override
+  void initState() {
+    context.read<MyLocationBloc>().startTracking();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    context.read<MyLocationBloc>().cancelTracking();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
